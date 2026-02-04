85afe21b2a170dadcee1f7c3b3b573dfebd9ef1c
gbingham18 <gbingham18@gmail.com>
2026-02-03 21:59:37 -0800

    Initial Commit

---
+++ PacketWatch/PacketWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacketWatchApp.swift
+//  PacketWatch
+//
+//  Created by Grant Bingham on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PacketWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
